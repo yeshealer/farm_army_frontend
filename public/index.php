109ae9c73a47a87b507473a0b85543cce9a81560
dosin <topfirepassion@gmail.com>
2021-12-16 05:13:50 +0100

    farm army frontend

---
+++ public/index.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Kernel;
+use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\ErrorHandler\Debug;
+use Symfony\Component\HttpFoundation\Request;
+
+require dirname(__DIR__).'/vendor/autoload.php';
+
+(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+
+if ($_SERVER['APP_DEBUG']) {
+    umask(0000);
+
+    Debug::enable();
+}
+
+$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+// Wrap the default Kernel with the CacheKernel one in 'prod' environment
+if ('prod' === $kernel->getEnvironment()) {
+    // $kernel = new \App\CacheKernel($kernel);
+}
+
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
